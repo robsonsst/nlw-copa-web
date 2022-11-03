f5bbbb4d2653118845255bfd350d494824824832
Robson Santos Santana <robsonsanttos_21@hotmail.com>
2022-11-03 13:01:32 -0300

    Terminando estilização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,60 +2,77 @@
   count: number;
 }*/
 
-import Image from 'next/image'
-import appPreviewImg from '../assets/app-nlw-copa-preview.png'
-import logoImg from '../assets/logo.svg'
-import usersAvatarExample from '../assets/users-avatar-example.png'
-import iconCheckImg from '../assets/icon-check.svg'
+import Image from "next/image";
+import appPreviewImg from "../assets/app-nlw-copa-preview.png";
+import logoImg from "../assets/logo.svg";
+import usersAvatarExample from "../assets/users-avatar-example.png";
+import iconCheckImg from "../assets/icon-check.svg";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
-        <Image src={logoImg} alt="NLW Copa"/>
+        <Image src={logoImg} alt="NLW Copa" />
 
-        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
 
-        <div>
-          <Image src={usersAvatarExample} alt="exemplo de usuários"/>
+        <div className="mt-10 flex items-center gap-2">
+          <Image src={usersAvatarExample} alt="exemplo de usuários" />
 
-          <strong>
-            <span>+12.592</span> pessoas já estão usando
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+12.592</span> pessoas já estão
+            usando
           </strong>
+        </div>
+        <form className="mt-10 flex gap-2">
+          <input
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+            type="text"
+            required
+            placeholder="Qual nome do seu bolão?"
+          />
+          <button
+            className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
+            type="submit"
+          >
+            Criar meu bolão
+          </button>
+        </form>
 
-          <form>
-            <input type="text" required placeholder='Qual nome do seu bolão?'/>
-            <button type='submit'>Criar meu bolão</button>
-          </form>
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu botão, você receberá um código único que poderá usar
+          para convidar outras pessoas 🚀
+        </p>
 
-          <p>Após criar seu botão, você receberá um código único que poderá usar para convidar outras pessoas </p>
-
-          <div>
-            <div>
-              <Image src={iconCheckImg} alt="ícone de check"/>
-              <div>
-                <span>+2.034</span>
-                <span>Bolões Criados</span>
-              </div>
+        <div className="mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100">
+          <div className="flex items-center gap-6">
+            <Image src={iconCheckImg} alt="ícone de check" />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+2.034</span>
+              <span>Bolões Criados</span>
             </div>
+          </div>
 
-            <div>
-              <Image src={iconCheckImg} alt="ícone de check"/>
-              <div>
-                <span>+192.847</span>
-                <span>Palpites enviados</span>
-              </div>
+          <div className="w-px h-14 bg-gray-600"/>
+
+          <div className="flex items-center gap-6">
+            <Image src={iconCheckImg} alt="ícone de check" />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+192.847</span>
+              <span>Palpites enviados</span>
             </div>
           </div>
         </div>
       </main>
-      <Image 
-        src={appPreviewImg} 
-        alt="Dois Celulares exibindo uma prévia da aplicação móvel do NLW Copa" 
+      <Image
+        src={appPreviewImg}
+        alt="Dois Celulares exibindo uma prévia da aplicação móvel do NLW Copa"
         quality={100}
       />
     </div>
-  )
+  );
 }
 /*      
 export const getServerSideProps = async () => {
